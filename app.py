c21e7cc977629276c894d162468c337bf61c6472
dhanush08041999 <dhanushvaradaraj079@gmail.com>
2026-08-24 16:51:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
-print("Hello from Python!")
-print("GitHub Actions is running my Python code!")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(b"""
+        <html>
+        <body>
+            <h1>Hello from GitHub Actions!</h1>
+            <h2>Deployed from ECR to EC2 🚀</h2>
+        </body>
+        </html>
+        """)
+
+server = HTTPServer(("0.0.0.0", 3000), Handler)
+
+print("Server running on port 3000")
+
+server.serve_forever()
